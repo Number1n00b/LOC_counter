4bc75894979b401d5d431db021116b14e5ad0254
Chris <cebrunette@gmail.com>
2017-08-25 22:59:55 +0800

    Fixed comment bug, added exclude directory option

--- src/LoCounter.java
+++ src/LoCounter.java
@@ -10,14 +10,17 @@ import java.util.Scanner;
 */
 public class LoCounter {
 
-   private static String usage = "Please run the program as follows:\n" +
-           "java LoCouter top_level_directory_name\n\n";
+   private static final String usage = "Please run the program as follows:\n" +
+           "java LoCouter top_level_directory_name -options\n\tFor help: java LoCounter -help\n";
 
 
    //todo - implement reading only specific file extensions
-   private static String extenderUsage = "Please run the program as follows:\n" +
+   private static final String extenderUsage = "Please run the program as follows:\n" +
            "java LoCouter top_level_directory_name -extensions a b c\n\n" +
            "Where 'a,b,c' are the file extensions of the files you want counted. e.g \".java\"";
+
+   private static final int MIN_ARGS = 1;
+   private static final int MAX_ARGS = 3;
 
    private static DecimalFormat f = new DecimalFormat("#0.00");
 
@@ -33,12 +36,28 @@ public class LoCounter {
 
    private static String[] extensions = {"java", "cpp", "hpp", "c", "h"};
 
+   private static String exclude_dir = "";
+
    public static void main(String[] args){
 
-      if(args.length == 1) {
+      if(args.length >= MIN_ARGS && args.length <= MAX_ARGS) {
          try {
             //String fileName = "F:\\Programming_Projects\\2016Sem2\\OOSE\\Assignment";
             String fileName = args[0];
+
+            System.out.println(args.length);
+            for(int ii = 0; ii < args.length; ii++){
+                System.out.println(ii + ") " + args[ii]);
+            }
+
+            if(args.length == 3){
+                if(args[1].equals("-exclude")){
+                    exclude_dir = args[2];
+                    System.out.println("Excluding: " + exclude_dir);
+                }else{
+                    System.out.println("Ignoring unknown arg: " + args[1] + "\n(Only supported arg is ''-exclude' to exclude a directory.) \n");
+                }
+            }
 
             File f = new File(fileName);
 
@@ -60,32 +79,36 @@ public class LoCounter {
    }
 
 
-   private static void recurseRead(File file) throws IOException{
-      //If the given file is actually a directory.
-      if( file.isDirectory() ) {
-         for ( final File fileEntry : file.listFiles() ) {
-            if ( fileEntry.isDirectory() && (!fileEntry.getName().endsWith(".git")) ) {
-               recurseRead(fileEntry);
-            } else {
-               if(hasValidExtension(fileEntry.getName())) {
-                  countLines(fileEntry);
+    private static void recurseRead(File file) throws IOException{
+        //If the given file is actually a directory.
+        if( file.isDirectory() ) {
+            if (!file.getName().equals(exclude_dir)){
+                System.out.println("========================\nReading directory: " + file.getName() + "\n========================");
+                // Read all files in this dir.
+                for ( final File fileEntry : file.listFiles() ) {
+                    if ( fileEntry.isDirectory() && (!fileEntry.getName().endsWith(".git")) ) {
+                        recurseRead(fileEntry);
+                    } else {
+                        if(hasValidExtension(fileEntry.getName())) {
+                            countLines(fileEntry);
+                            System.out.println("File: " + fileEntry.getName());
+                            printNumLines();
+                            numFiles++;
+                            TotalNumBlankLines += numBlankLines;
+                            TotalNumCodeLines += numCodeLines;
+                            TotalNumCommentLines += numCommentLines;
 
-                  printNumLines();
-                  numFiles++;
-                  TotalNumBlankLines += numBlankLines;
-                  TotalNumCodeLines += numCodeLines;
-                  TotalNumCommentLines += numCommentLines;
-
-                  numBlankLines = 0;
-                  numCodeLines = 0;
-                  numCommentLines = 0;
-               }
+                            numBlankLines = 0;
+                            numCodeLines = 0;
+                            numCommentLines = 0;
+                        }
+                    }
+                }
             }
-         }
-      }else{
-         countLines(file);
-      }
-   }
+        }else{
+            countLines(file);
+        }
+    }
 
    private static boolean hasValidExtension(String name){
       for(String valid : extensions){
@@ -99,8 +122,6 @@ public class LoCounter {
 
    //Guaranteed to be a file at this point and not a directory, so start counting lines.
    private static void countLines(File f) throws IOException {
-      System.out.println("File: " + f.getName());
-
       Scanner sc = new Scanner(f);
 
       String currLine;
@@ -115,8 +136,14 @@ public class LoCounter {
          if(inComment){
             codeLine = false;
             int ii = 0;
+
+            //Eat leading spaces.
+            while(currLine.charAt(ii) == ' '){
+                ii++;
+            }
+
             char currChar;
-            char prevChar = '=';
+            char prevChar = '='; //Assign to random char ???
             while( (ii < currLine.length()) && inComment) {
                currChar = currLine.charAt(ii);
                //Check for exit comment
@@ -137,42 +164,58 @@ public class LoCounter {
          }
          else{
             boolean done = false;
-            //while I haven't determined what the line is, keep trying
+
             char currChar;
-            char prevChar = '+';
+            char prevChar = '+'; //Assign to random char ???
             int ii = 0;
+
+            //Eat leading spaces.
+            while(currLine.charAt(ii) == ' '){
+                ii++;
+            }
+
+            //while I haven't determined what the line is, keep trying
             while( (ii < currLine.length()) && !done){
                currChar = currLine.charAt(ii);
 
-               //Check single line comment
-               if( (currChar == '/') && (prevChar == '/') ){
-                  numCommentLines++;
-                  codeLine = false;
-                  done = true;
-               }
-               //Check multiline comment
-               else if((currChar == '*') && (prevChar == '/')){
-                  //Internal loop to check for end of comment
-                  while( (ii < currLine.length())){
-                     inComment = true;
-                     currChar = currLine.charAt(ii);
-                     //Check for exit comment
-                     if( (currChar == '/') && (prevChar == '*') ){
-                        inComment = false;
-                     }
+               //If the first non-space character is not a slash, it is definately
+               //not a comment starting.
+               if(currChar != '/'){
+                   codeLine = true;
+                   done = true;
+               }else{
+                   //Check single line comment
+                   if( (currChar == '/') && (prevChar == '/') ){
+                      numCommentLines++;
+                      codeLine = false;
+                      done = true;
+                   }
+                   //Check multiline comment
+                   else if((currChar == '*') && (prevChar == '/')){
+                      //Internal loop to check for end of comment
+                      while( (ii < currLine.length())){
+                         inComment = true;
+                         currChar = currLine.charAt(ii);
+                         //Check for exit comment
+                         if( (currChar == '/') && (prevChar == '*') ){
+                            inComment = false;
+                         }
 
-                     prevChar = currChar;
-                     ii++;
-                  }
+                         prevChar = currChar;
+                         ii++;
+                      }
 
-                  numCommentLines++;
-                  codeLine = false;
+                      numCommentLines++;
+                      codeLine = false;
+                   }
                }
 
                prevChar = currChar;
                ii++;
             }
-         }if(codeLine){
+         }
+
+         if(codeLine){
             numCodeLines++;
          }
       }
@@ -209,11 +252,13 @@ public class LoCounter {
       int total = (TotalNumBlankLines + TotalNumCommentLines + TotalNumCodeLines);
 
       double percentCode = ((double)TotalNumCodeLines / (double)total);
+      double percentComment = ((double)TotalNumCommentLines / (double)total);
+      double percentBlank = ((double)TotalNumBlankLines / (double)total);
 
-      System.out.println("Blank: " + TotalNumBlankLines);
-      System.out.println("Comments: " + TotalNumCommentLines);
-      System.out.println("Code: " + TotalNumCodeLines);
-      System.out.println("------------\nTotal (" + numFiles + " files read): " + total + " ( " + f.format(percentCode) + "% code)\n------------" );
+      System.out.println("Blank: " + TotalNumBlankLines + " (" + f.format(percentBlank*100) + "%)");
+      System.out.println("Comments: " + TotalNumCommentLines + " (" + f.format(percentComment*100) + "%)");
+      System.out.println("Code: " + TotalNumCodeLines + " (" + f.format(percentCode*100) + "%)");
+      System.out.println("------------\nTotal lines (" + numFiles + " files read): " + total + "\n------------" );
 
    }
 }
